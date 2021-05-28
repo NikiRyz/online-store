fd7785c7e04e0bd3801cb02af67b49065f00a34b
Рязанова Вероника Кирилловна <VKRyazanova@sberbank.ru>
2021-05-28 11:48:52 +0300

    15

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize') //импортировали sequelize
 
 module.exports = new Sequelize(
-    'df01rak35ep7lc', // Название БД
-    'bxwkaipkydiasb', // Пользователь
-    '1a44c043966e06adb9a2d4c5628b9c18440001fd24ee6627bef3486ac6f62a2b', // ПАРОЛЬ
+    'online_store', // Название БД
+    'postgres', // Пользователь
+    'кщще', // ПАРОЛЬ
     {
         dialect: 'postgres',
         port: 5432
